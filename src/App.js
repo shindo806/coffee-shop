081f45f5a01018d6fa83360c76cdc144706e8431
unknown <thaisonlamsp@gmail.com>
2019-11-27 21:59:39 +0700

    initial

--- src/App.js
+++ src/App.js
@@ -1,11 +1,30 @@
 import React from 'react';
 
 import './Styles/main.scss';
+import MainButton from './Components/Button/Button';
+
 
 function App() {
   return (
     <div className="App">
       <h1>Hello World</h1>
+      <MainButton />
+      <div>
+        <div className="ui right labeled button" role="button" tabindex="0">
+          <button className="ui red button">
+            <i aria-hidden="true" className="heart icon"></i>
+            Like
+    </button>
+          <a className="ui red left pointing basic label">2,048</a>
+        </div>
+        <div className="ui right labeled button" role="button" tabindex="0">
+          <button className="ui blue basic button">
+            <i aria-hidden="true" className="fork icon"></i>
+            Fork
+    </button>
+          <a className="ui blue left pointing basic label">2,048</a>
+        </div>
+      </div>
     </div>
   );
 }
