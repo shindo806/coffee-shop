28f7df7cd91428b84acddf937d17a677181aec5d
shindo806 <thaisonlamsp@gmail.com>
2019-11-27 22:46:44 +0700

    add semantic-ui framework

--- src/App.js
+++ src/App.js
@@ -1,30 +1,14 @@
 import React from 'react';
 
+import 'semantic-ui-css/semantic.min.css';
 import './Styles/main.scss';
-import MainButton from './Components/Button/Button';
+import Navbar from './Components/Menu'
 
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello World</h1>
-      <MainButton />
-      <div>
-        <div className="ui right labeled button" role="button" tabindex="0">
-          <button className="ui red button">
-            <i aria-hidden="true" className="heart icon"></i>
-            Like
-    </button>
-          <a className="ui red left pointing basic label">2,048</a>
-        </div>
-        <div className="ui right labeled button" role="button" tabindex="0">
-          <button className="ui blue basic button">
-            <i aria-hidden="true" className="fork icon"></i>
-            Fork
-    </button>
-          <a className="ui blue left pointing basic label">2,048</a>
-        </div>
-      </div>
+      <Navbar />
     </div>
   );
 }
--- src/Components/Button/Button.js
+++
@@ -1,6 +0,0 @@
-import React from 'react'
-import { Button } from 'semantic-ui-react'
-
-const MainButton = () => <Button primary>Click Here</Button>
-
-export default MainButton
